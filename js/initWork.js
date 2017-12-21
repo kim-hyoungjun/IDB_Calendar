08581e3ce01b30195e168a774f183d3550fb135e
kim-hyoungjun <decoy1972@hotmail.com>
2017-12-21 17:20:12 +0900

    Event Add

--- js/initWork.js
+++ js/initWork.js
@@ -1,4 +1,41 @@
-﻿function init_variable() {
+﻿Date.prototype.format30 = function(f) {
+	if (!this.valueOf()) return " ";
+ 
+	var weekName = ["일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일"];
+	var d = this;
+//	var d = new Date();
+	var p = 60 - d.getMinutes();
+	if (p > 30) p = p - 30;
+	d.setMinutes(d.getMinutes() + p);
+     
+	return f.replace(/(yyyy|yy|MM|dd|E|hh|mm|ss|a\/p)/gi, function($1) {
+		switch ($1) {
+			case "yyyy": return d.getFullYear();
+			case "yy": return (d.getFullYear() % 1000).zf(2);
+			case "MM": return (d.getMonth() + 1).zf(2);
+			case "dd": return d.getDate().zf(2);
+			case "E": return weekName[d.getDay()];
+			case "HH": return d.getHours().zf(2);
+			case "hh": return ((h = d.getHours() % 12) ? h : 12).zf(2);
+			case "mm": return d.getMinutes().zf(2);
+			case "ss": return d.getSeconds().zf(2);
+			case "a/p": return d.getHours() < 12 ? "오전" : "오후";
+			default: return $1;
+		}
+	});
+};
+ 
+String.prototype.string = function(len){var s = '', i = 0; while (i++ < len) { s += this; } return s;};
+String.prototype.add = function(val) {
+	var tmpVal = parseInt(this, 10);
+	if (tmpVal < 23) tmpVal ++;
+		
+	return tmpVal.toString().zf(2);
+}
+String.prototype.zf = function(len){return "0".string(len - this.length) + this;};
+Number.prototype.zf = function(len){return this.toString().zf(len);};
+
+function init_variable() {
 	var mm1	= "00";
 	var mm2	= "30";
 
@@ -38,5 +75,13 @@
 }
 
 function fn_getToDay() {
+	var _date		= new Date().format30("yyyy-MM-dd");
+	var _time1	= new Date().format30("HH:mm");
+	var _cTime	= new Date().format30("HH");
+	var _time2	= new Date().format30("HH").add(1) + _time1.substring(2,5);
 	
+	$("#sd").val(_date);
+	$("#ed").val(_date);
+	$("#sdt").val(_time1);
+	$("#edt").val(_time2);
 }
